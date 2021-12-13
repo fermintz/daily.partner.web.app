c78d564719c4fba1de8ef434b7852790478344ae
fermintz <fermintz@gmail.com>
2021-12-13 17:23:38 +0900

    오더카드

--- src/components/orderCard.vue
+++ src/components/orderCard.vue
@@ -2,41 +2,53 @@
   <div class="item" @click.stop="$router.push('orderDetail')">
     <div class="item_top">
       <div class="state">
-        <strong>반품</strong>
-        <span>{{state}}</span>
-        
+        <span class="refound">반품</span>
+        <span class="state_name">{{state}}</span>
       </div>
-      <div class="amount">
-        <span>상품</span>
-        <strong>72</strong>
-      </div>
-      <div class="right">
-        <div class="number">48164</div>
-        <div class="in_date">
-          <span>입고일</span>
-          <strong>2021.09.30 (목)</strong>
-        </div>
+      <div class="order_date">
+        <span>입고일</span>
+        <strong>21.10.31</strong>
       </div>
     </div>
-    <div class="message" v-show="!message">
-      <span>청바지 접어 놓았어요 바지좀 줄여주세요!</span>
+
+    <div class="item_middle">
+      <div class="d-row">
+        <dl class="order_amount">
+          <dt>상품</dt>
+          <dd>72</dd>
+        </dl>
+        <dl class="order_number">
+          <dt>주문번호</dt>
+          <dd>3IHH27-<b>8D50</b></dd>
+        </dl>
+      </div>
+
+      <div class="message" v-show="!message">
+        <strong>고객요청</strong>
+        <span>명품류 주의요청(단추/와펜 등) <br/><br/>청바지 접어 놓았어요 바지좀 줄여주세요!</span>
+      </div>
+      <div class="message cs" v-show="!csMessage">
+        <strong>검토안내</strong>
+        <span>
+          오염이 심한 와이셔츠는 1개 취소. 나머지 상품만 작업시작
+        </span>
+      </div>
     </div>
-    <div class="cs_message" v-show="!csMessage">
-      <strong>검토안내</strong>
-      <span>
-        오염이 심한 와이셔츠는 1개 취소. 나머지 상품만 작업시작
-      </span>
-    </div>
+
     <div class="bottom_btns" v-show="!btns">
-      <v-btn text class="phone">
-        <v-icon>mdi-phone</v-icon>
-        <span>전화</span>
-      </v-btn>
-      <v-btn text class="next" @click.stop="$router.push('checkOrder')">
-        <span>{{next}}</span>
-        <v-icon>mdi-arrow-right</v-icon>
-      </v-btn>
+      <div class="btn_inner">
+        <v-btn text class="phone">
+          <v-icon>mdi-phone</v-icon>
+          <span>전화</span>
+        </v-btn>
+        <v-btn text class="next" @click.stop="$router.push('checkOrder')">
+          <span>{{next}}</span>
+          <v-icon>mdi-arrow-right</v-icon>
+        </v-btn>
+      </div>
     </div>
+    
+    
 
   </div> <!-- item -->
 </template>
@@ -55,118 +67,123 @@ export default {
   background:#fff;
   border-top:1px solid #e2e2e2;
   border-bottom:1px solid #c2c2c2;
-  min-height:100px;
-  padding:15px;
+
   
   .item_top{
     display:flex;
-    .state{
-      display:Flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
-      height:50px;
-      background:#292929;
-      padding:0 10px;
-      border-radius:5px;
-      color:#ffff;     
-      margin-right:5px;
+    align-items: center;
+    justify-content: space-between;
+    height:44px;
+    border-bottom:1px solid #e2e2e2;
+    padding:0 15px;
 
-      span{
-        font-size:13px;
-        line-height:1;
-        font-weight:bold;
-      }
-      strong{
-        font-weight:normal;
-        font-size:11px;
-        line-height:1;
-        color:#FFC400;
-        margin-bottom:5px;
-      }
-    }
-
-    .amount{
-      display:flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      height:50px;
-      width:50px;
-      background:#EBF1F5;
-      border-radius:5px;
-
+    .order_date{
       span{
         font-size:10px;
-        margin-bottom:2px;
-        font-weight:bold;
+        color:#898989;
+        margin-right:10px;
       }
-      strong{
-        font-size:18px;
-        line-height: 1;
-        color:#008BE8;
-      ;}
     }
-    .right{
+    .state{
       display:flex;
-      flex-direction: column;
-      justify-content: center;
-      margin-left:15px;
-      .number{
-        font-size:20px;
-        line-height: 1;
-        font-weight:500;
-      }
-      .in_date{
+      align-items: center;
+      
+      span{
         display:flex;
-        margin-top:5px;
         align-items: center;
-        font-size:12px;
-        span{color:#898989;}
-        strong{margin-left:5px;font-weight:normal;}
+        justify-content: center;
+        height:20px;
+        padding:0 5px;
+        color:#fff;
+        border-radius:3px;
+        margin-right:5px;
+        font-size:10px;
+      }
+      .refound{
+        background:#d22828;
+      }
+      .state_name{
+        background:#008BE8;
       }
     }
   }
 
-  .message{
-    margin-top:15px;
-    span{
-      display:block;
-      padding:5px 10px;
-      background:#F8F6F1;
-      margin-bottom:5px;
+  .item_middle{
+    padding:15px;
+    .d-row{
+      display:flex;
+      align-items: center;
+      dl{
+        margin-right:15px;
+        dt{
+          font-size:10px;
+          color:#898989;
+        }
+        dd{
+          font-size:16px;
+          line-height:1;
+          margin-top:2px;
+          font-weight:bold;
+        }
+      }
+      dl.order_number{
+        dd{
+          b{
+            color:#008BE8
+          }
+        }
+      }
+      dl.order_amount{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width:56px;
+        height:56px;
+        border:1px solid #e2e2e2;
+        border-radius:5px;
+
+      }
+    }
+
+    .message{
+      display:flex;
+      background:#FBF5F7;
+      padding:10px;
       border-radius:5px;
-      font-size:13px;
+      margin-top:10px;
+
+      strong{
+        color:#d22828;
+        font-size:12px;
+        margin-right:20px;
+      }
+      span{
+        font-size:12px;
+        flex:1;
+      }
     }
-    span:last-child{
-      margin-bottom:0px;
+
+    .message.cs{
+      background:#f8f8f8;
+      strong{
+        color:#16AF80
+      }
     }
+
   }
 
-  .cs_message{
-    display:flex;
-    border:1px solid #16AF80;
-    padding:10px;
-    border-radius:5px;
-    margin-top:15px;
-
-    strong{
-      color:#16AF80;
-      font-size:13px;
-      margin-right:15px;
-    }
-    span{
-      font-size:13px;
-      flex:1;
-    }
-  }
-
+  
   .bottom_btns{
-    display:flex;
-    border:1px solid #c2c2c2;
-    margin-top:15px;
-    border-radius:5px;
-    overflow:hidden;
+    padding:10px;
+    padding-top:0px;
+
+    .btn_inner{
+      display:flex;
+      border:1px solid #c2c2c2;
+      border-radius:5px;
+      overflow:hidden;
+    }
     
     .v-btn{
       fleX:1;
